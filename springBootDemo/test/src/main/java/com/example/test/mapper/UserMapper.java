aaaff2de744e67c411f86b3da2e49af6d553f122
wt <1546766776@qq.com>
2021-01-13 15:48:20 +0800

    开始springBoot的学习了

---
+++ springBootDemo/test/src/main/java/com/example/test/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.example.test.mapper;
+
+import com.example.test.bean.UserBean;
+
+public interface UserMapper {
+
+    UserBean getInfo(String name,String password);
+}
---
+++ springBootDemo/test/src/test/java/com/example/test/TestApplicationTests.java
@@ -0,0 +1,25 @@
+package com.example.test;
+
+import com.example.test.bean.UserBean;
+import com.example.test.service.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestApplicationTests {
+
+    @Autowired
+    UserService userService;
+
+    @Test
+    public void contextLoads() {
+        UserBean userBean = userService.loginIn("张三","a123");
+        System.out.println(userBean.toString());
+        System.out.println(userBean.getId());
+    }
+
+}
